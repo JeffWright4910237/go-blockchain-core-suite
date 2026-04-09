3349882d0d9d0b2bbf883f8ab81b13b5702e6338
JeffWright4910237 <JeffWright4910237@cdzhongguan.com>
2026-04-09 19:54:23 +0800

    Create sha256_hash_tool.go

---
+++ sha256_hash_tool.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func SHA256Hash(data string) string {
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
+}
+
+func DoubleSHA256(data string) string {
+	first := sha256.Sum256([]byte(data))
+	second := sha256.Sum256(first[:])
+	return hex.EncodeToString(second[:])
+}
